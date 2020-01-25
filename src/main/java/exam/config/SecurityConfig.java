ac3e7e34a5320962be5111a8379ecdf8e2a79102
camila-soares <camilasoares1507@gmail.com>
2020-01-25 11:52:07 -0300

    oauth2

--- src/main/java/exam/config/SecurityConfig.java
+++ src/main/java/exam/config/SecurityConfig.java
@@ -1,23 +1,27 @@
-//package exam.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().
-//        disable()
-//            .authorizeRequests()
-//            .antMatchers(HttpMethod.OPTIONS, "/**")
-//            .permitAll()
-//            .anyRequest()
-//            .authenticated()
-//            .and()
-//            .httpBasic();
-//	}
-//}
+package exam.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+	
+	@Override
+	   protected void configure(HttpSecurity http) throws Exception {
+	      http
+	         .csrf()
+	         .disable()
+	         //.antMatcher("/**")
+	         .authorizeRequests()
+	         .antMatchers(HttpMethod.OPTIONS, "/pessoa")
+	         .permitAll()
+	         .anyRequest()
+	         .authenticated();
+	   }
+}
