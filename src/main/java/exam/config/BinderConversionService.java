21a6ce292566bde184047b1662b9f9a388807d5b
camila-soares <camilasoares1507@gmail.com>
2020-01-25 13:41:03 -0300

    implementacao oauth2 integrado com google

---
+++ src/main/java/exam/config/BinderConversionService.java
@@ -0,0 +1,16 @@
+package exam.config;
+
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+
+public class BinderConversionService {
+
+	public BinderConversionService(Object object) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommonOAuth2Provider convert(String providerId, Class<CommonOAuth2Provider> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
