c64391880f7b89f8283a69bd0b2c75ba7a34b21e
camila-soares <camilasoares1507@gmail.com>
2020-01-25 12:56:12 -0300

    implementacao oauth2 integrado com google

--- src/main/java/exam/ExamApplication.java
+++ src/main/java/exam/ExamApplication.java
@@ -9,10 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import exam.repository.PessoaRepository;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class ExamApplication implements CommandLineRunner {
 
 	@Autowired
@@ -21,6 +27,11 @@ private PessoaRepository pessoaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ExamApplication.class, args);
 		}
+	
+	@Bean
+    public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ProtectedResourceDetails resource){
+        return new OAuth2RestTemplate(resource);
+    }
 
 	@Override
 	public void run(String... args) throws Exception {
